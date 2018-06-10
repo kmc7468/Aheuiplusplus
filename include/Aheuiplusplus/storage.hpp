e07d96751d2bfa190f3f527d3f677f491bf4345c
static <kmc7468@naver.com>
2018-06-11 01:23:16 +0900

    list 및 queue 클래스 추가
    이 클래스들은 storage 클래스를 상속 받은 클래스로, 각각 내부적으로 std::vector, std::queue를 저장함으로써 아희++에서의 리스트 및 큐를 구현합니다.

--- include/Aheuiplusplus/storage.hpp
+++ include/Aheuiplusplus/storage.hpp
@@ -5,7 +5,9 @@
 #include <Aheuiplusplus/function.hpp>
 
 #include <cstddef>
+#include <queue>
 #include <variant>
+#include <vector>
 
 namespace app
 {
@@ -21,10 +23,12 @@ namespace app
 	class storage
 	{
 	public:
-		storage() = default;
 		storage(const storage& storage) = delete;
 		storage(storage&& storage) noexcept = delete;
 		virtual ~storage() = default;
+
+	protected:
+		storage() = default;
 
 	public:
 		storage& operator=(const storage& storage) = delete;
@@ -37,13 +41,59 @@ namespace app
 		virtual element* pop() = 0;
 		virtual void push(element* element) = 0;
 		virtual std::size_t length() const = 0;
+	};
+
+	class list final : public storage
+	{
+	public:
+		list() = default;
+		list(const list& storage) = delete;
+		list(list&& storage) noexcept = delete;
+		virtual ~list() override = default;
+
+	public:
+		list& operator=(const list& storage) = delete;
+		list& operator=(list&& storage) noexcept = delete;
+		bool operator==(const list& storage) const = delete;
+		bool operator!=(const list& storage) const = delete;
+
+	public:
+		virtual storage_type type() const noexcept override;
+		virtual element* pop() override;
+		virtual void push(element* element) override;
+		virtual std::size_t length() const override;
 
 	public:
 		std::size_t virtual_length() const noexcept;
-		void virtual_length(std::size_t new_virtual_length) noexcept;
+		void virtual_length(std::size_t new_virtual_length);
 
 	private:
+		std::vector<element*> vector_;
 		std::size_t virtual_length_;
+	};
+
+	class queue final : public storage
+	{
+	public:
+		queue() = default;
+		queue(const queue& storage) = delete;
+		queue(queue&& storage) noexcept = delete;
+		virtual ~queue() override = default;
+
+	public:
+		queue& operator=(const queue& storage) = delete;
+		queue& operator=(queue&& storage) noexcept = delete;
+		bool operator==(const queue& storage) const = delete;
+		bool operator!=(const queue& storage) const = delete;
+
+	public:
+		virtual storage_type type() const noexcept override;
+		virtual element* pop() override;
+		virtual void push(element* element) override;
+		virtual std::size_t length() const override;
+
+	private:
+		std::queue<element*> queue_;
 	};
 }
 
--- src/storage.cpp
+++ src/storage.cpp
@@ -1,13 +1,70 @@
 #include <Aheuiplusplus/storage.hpp>
 
+#include <stdexcept>
+
 namespace app
 {
-	std::size_t storage::virtual_length() const noexcept
+	storage_type list::type() const noexcept
+	{
+		return storage_type::list;
+	}
+	element* list::pop()
+	{
+		std::vector<element*>::iterator end = vector_.begin() + virtual_length();
+
+		if (vector_.begin() == end)
+		{
+			return nullptr;
+		}
+
+		element* data = *(end - 1);
+		vector_.erase(end - 1);
+		virtual_length(virtual_length() - 1);
+
+		return data;
+	}
+	void list::push(element* element)
+	{
+		vector_.push_back(element);
+		virtual_length(virtual_length() + 1);
+	}
+	std::size_t list::length() const
+	{
+		return vector_.size();
+	}
+
+	std::size_t list::virtual_length() const noexcept
 	{
 		return virtual_length_;
 	}
-	void storage::virtual_length(std::size_t new_virtual_length) noexcept
+	void list::virtual_length(std::size_t new_virtual_length)
 	{
+		if (new_virtual_length > length())
+			throw std::out_of_range("인수 new_virtual_length는 함숫값 length()보다 클 수 없습니다.");
+
 		virtual_length_ = new_virtual_length;
+	}
+}
+
+namespace app
+{
+	storage_type queue::type() const noexcept
+	{
+		return storage_type::queue;
+	}
+	element* queue::pop()
+	{
+		element* data = queue_.front();
+		queue_.pop();
+
+		return data;
+	}
+	void queue::push(element* element)
+	{
+		queue_.push(element);
+	}
+	std::size_t queue::length() const
+	{
+		return queue_.size();
 	}
 }
