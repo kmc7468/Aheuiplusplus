ed73dc3db7f304ec52ff46f9ccccce906e84fc37
static <kmc7468@naver.com>
2018-06-11 01:28:38 +0900

    interpreter 클래스에 storages 필드 추가
    이 필드는 저장공간을 저장합니다.

--- include/Aheuiplusplus/interpreter.hpp
+++ include/Aheuiplusplus/interpreter.hpp
@@ -2,6 +2,7 @@
 #define AHEUIPLUSPLUS_HEADER_INTERPRETER_HPP
 
 #include <Aheuiplusplus/function.hpp>
+#include <Aheuiplusplus/storage.hpp>
 #include <Aheuiplusplus/version.hpp>
 
 #include <vector>
@@ -25,10 +26,16 @@ namespace app
 
 	public:
 		app::version version() const noexcept;
+		const std::vector<std::vector<app::storage*>>& storages() const noexcept;
+		const app::storage* storage(std::size_t index) const;
+		const std::vector<std::size_t>& storage_indexs() const noexcept;
+		std::size_t storage_index(std::size_t index) const;
 		const std::vector<function*>& functions() const noexcept;
 
 	private:
 		app::version version_ = app::version::v1_0;
+		std::vector<std::vector<app::storage*>> storages_;
+		std::vector<std::size_t> storage_indexs_;
 		std::vector<function*> functions_;
 	};
 }
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -10,8 +10,24 @@ namespace app
 	{
 		return version_;
 	}
+	const std::vector<std::vector<app::storage*>>& interpreter::storages() const noexcept
+	{
+		return storages_;
+	}
+	const app::storage* interpreter::storage(std::size_t index) const
+	{
+		return storages_[index][storage_indexs_[index]];
+	}
 	const std::vector<function*>& interpreter::functions() const noexcept
 	{
 		return functions_;
+	}
+	const std::vector<std::size_t>& interpreter::storage_indexs() const noexcept
+	{
+		return storage_indexs_;
+	}
+	std::size_t interpreter::storage_index(std::size_t index) const
+	{
+		return storage_indexs_[index];
 	}
 }
