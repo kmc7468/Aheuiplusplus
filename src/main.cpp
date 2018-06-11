8bff05c3b6d63b1e4617f5701158ac6e60bef838
static <kmc7468@naver.com>
2018-06-11 17:15:52 +0900

    플랫폼 검증 추가
    만약 wchar_t와 char32_t 타입의 크기가 서로 다를 경우, 플랫폼이 윈도우인 경우에만 컴파일 되도록 플랫폼 검증을 추가하였습니다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-﻿int main(int argc, char** argv)
+﻿#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#	define AHEUIPLUSPLUS_MACRO_IS_WINDOWS true
+#else
+#	define AHEUIPLUSPLUS_MACRO_IS_WINDOWS false
+#endif
+
+static_assert(sizeof(wchar_t) != sizeof(char32_t) && AHEUIPLUSPLUS_MACRO_IS_WINDOWS,
+	"Only Windows can have different sizes of wchar_t and char32_t.");
+
+int main(int argc, char** argv)
 {
 	return 0;
 }
