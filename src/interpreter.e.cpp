1753c1f91e7f092cfa8db5a29110c5cc5aa7176b
static <kmc7468@naver.com>
2018-06-13 01:23:42 +0900

    interpreter::run 메서드의 ㄷ 명령에 형 변환 구현
    기존 구현에서는 숫자 + 숫자 및 숫자 | 숫자 연산만 가능했으나, 현재 구현에서는 표준에서 허용하는 모든 덧셈 및 비트 OR 연산을 수행할 수 있습니다.

--- src/interpreter.e.cpp
+++ src/interpreter.e.cpp
@@ -9,23 +9,170 @@ namespace app
 			element* right_operand = storage_()->pop();
 			element* left_operand = storage_()->pop();
 
-			if (is_integer_mode_)
+			if (left_operand->index() == right_operand->index())
 			{
-				long long right_operand_integer = std::get<0>(*right_operand).integer();
-				long long left_operand_integer = std::get<0>(*left_operand).integer();
+				switch (left_operand->index())
+				{
+				case 0: // 숫자
+				{
+					if (is_integer_mode_)
+					{
+						long long right_operand_integer = std::get<0>(*right_operand).integer();
+						long long left_operand_integer = std::get<0>(*left_operand).integer();
 
-				long long result = left_operand_integer + right_operand_integer;
+						long long result = left_operand_integer + right_operand_integer;
 
-				storage_()->push(new element(number(result)));
+						storage_()->push(new element(number(result)));
+					}
+					else
+					{
+						double right_operand_decimal = std::get<0>(*right_operand).decimal();
+						double left_operand_decimal = std::get<0>(*left_operand).decimal();
+
+						double result = left_operand_decimal + right_operand_decimal;
+
+						storage_()->push(new element(number(result)));
+					}
+
+					break;
+				}
+
+				case 1: // 문자
+				{
+					char32_t right_operand_char = std::get<1>(*right_operand);
+					char32_t left_operand_char = std::get<1>(*left_operand);
+
+					char32_t result = left_operand_char + right_operand_char;
+
+					storage_()->push(new element(result));
+
+					break;
+				}
+
+				case 2: // 문자열
+				{
+					raw_code right_operand_string = std::get<2>(*right_operand);
+					raw_code left_operand_string = std::get<2>(*right_operand);
+
+					storage_()->push(new element(left_operand_string + right_operand_string));
+
+					break;
+				}
+				}
 			}
 			else
 			{
-				double right_operand_decimal = std::get<0>(*right_operand).decimal();
-				double left_operand_decimal = std::get<0>(*left_operand).decimal();
+				switch (left_operand->index())
+				{
+				case 0:
+				{
+					switch (right_operand->index())
+					{
+					case 1: // 숫자 + 문자 = 숫자
+					{
+						if (is_integer_mode_)
+						{
+							long long left_operand_integer = std::get<0>(*left_operand).integer();
+							long long right_operand_integer = static_cast<long long>(std::get<1>(*right_operand));
+						
+							long long result = left_operand_integer + right_operand_integer;
 
-				double result = left_operand_decimal + right_operand_decimal;
+							storage_()->push(new element(number(result)));
+						}
+						else
+						{
+							double left_operand_decimal = std::get<0>(*left_operand).decimal();
+							double right_operand_decimal = static_cast<double>(std::get<1>(*right_operand));
 
-				storage_()->push(new element(number(result)));
+							double result = left_operand_decimal + right_operand_decimal;
+
+							storage_()->push(new element(number(result)));
+						}
+
+						break;
+					}
+
+					case 2: // 숫자 + 문자열 = 문자열
+					{
+						char32_t left_operand_char = static_cast<char32_t>(std::get<0>(*left_operand).integer());
+						raw_code right_operand_string = std::get<2>(*right_operand);
+
+						storage_()->push(new element(left_operand_char + right_operand_string));
+
+						break;
+					}
+					}
+					break;
+				}
+
+				case 1:
+				{
+					switch (right_operand->index())
+					{
+					case 0: // 문자 + 숫자 = 숫자
+					{
+						if (is_integer_mode_)
+						{
+							long long left_operand_integer = static_cast<long long>(std::get<1>(*left_operand));
+							long long right_operand_integer = std::get<0>(*right_operand).integer();
+
+							long long result = left_operand_integer + right_operand_integer;
+
+							storage_()->push(new element(number(result)));
+						}
+						else
+						{
+							double left_operand_decimal = static_cast<double>(std::get<1>(*left_operand));
+							double right_operand_decimal = std::get<0>(*right_operand).decimal();
+
+							double result = left_operand_decimal + right_operand_decimal;
+
+							storage_()->push(new element(number(result)));
+						}
+
+						break;
+					}
+
+					case 2: // 문자 + 문자열 = 문자열
+					{
+						char32_t left_operand_char = std::get<1>(*left_operand);
+						raw_code right_operand_string = std::get<2>(*right_operand);
+
+						storage_()->push(new element(left_operand_char + left_operand_char));
+
+						break;
+					}
+					}
+					break;
+				}
+
+				case 2:
+				{
+					switch (right_operand->index())
+					{
+					case 0: // 문자열 + 숫자 = 문자열
+					{
+						raw_code left_operand_string = std::get<2>(*left_operand);
+						char32_t right_operand_char = static_cast<char32_t>(std::get<0>(*right_operand).integer());
+
+						storage_()->push(new element(left_operand_string + right_operand_char));
+
+						break;
+					}
+
+					case 1: // 문자열 + 문자 = 문자열
+					{
+						raw_code left_operand_string = std::get<2>(*left_operand);
+						char32_t right_operand_char = std::get<1>(*right_operand);
+
+						storage_()->push(new element(left_operand_string + right_operand_char));
+
+						break;
+					}
+					}
+					break;
+				}
+				}
 			}
 
 			delete right_operand;
@@ -38,28 +185,110 @@ namespace app
 			element* right_operand = storage_()->pop();
 			element* left_operand = storage_()->pop();
 
-			if (is_integer_mode_)
+			if (left_operand->index() == right_operand->index())
 			{
-				long long right_operand_integer = std::get<0>(*right_operand).integer();
-				long long left_operand_integer = std::get<0>(*left_operand).integer();
+				switch (left_operand->index())
+				{
+				case 0: // 숫자
+				{
+					if (is_integer_mode_)
+					{
+						long long right_operand_integer = std::get<0>(*right_operand).integer();
+						long long left_operand_integer = std::get<0>(*left_operand).integer();
 
-				long long result = left_operand_integer | right_operand_integer;
+						long long result = left_operand_integer | right_operand_integer;
 
-				storage_()->push(new element(number(result)));
+						storage_()->push(new element(number(result)));
+					}
+					else
+					{
+						double right_operand_decimal = std::get<0>(*right_operand).decimal();
+						double left_operand_decimal = std::get<0>(*left_operand).decimal();
+
+						static_assert(sizeof(long long) == sizeof(double), "The sizes of long long and double types must be the same.");
+
+						long long right_operand_integer = *reinterpret_cast<long long*>(&right_operand_decimal);
+						long long left_operand_integer = *reinterpret_cast<long long*>(&left_operand_decimal);
+
+						long long result = left_operand_integer + right_operand_integer;
+
+						storage_()->push(new element(number(result)));
+					}
+
+					break;
+				}
+
+				case 1: // 문자
+				{
+					char32_t right_operand_char = std::get<1>(*right_operand);
+					char32_t left_operand_char = std::get<1>(*left_operand);
+
+					char32_t result = left_operand_char | right_operand_char;
+
+					storage_()->push(new element(result));
+
+					break;
+				}
+				}
 			}
 			else
 			{
-				double right_operand_decimal = std::get<0>(*right_operand).decimal();
-				double left_operand_decimal = std::get<0>(*left_operand).decimal();
+				switch (left_operand->index())
+				{
+				case 0: // 숫자 | 문자 = 숫자
+				{
+					if (is_integer_mode_)
+					{
+						long long right_operand_integer = static_cast<long long>(std::get<1>(*right_operand));
+						long long left_operand_integer = std::get<0>(*left_operand).integer();
 
-				static_assert(sizeof(long long) == sizeof(double), "The sizes of long long and double types must be the same.");
+						long long result = left_operand_integer | right_operand_integer;
 
-				long long right_operand_integer = *reinterpret_cast<long long*>(&right_operand_decimal);
-				long long left_operand_integer = *reinterpret_cast<long long*>(&left_operand_decimal);
+						storage_()->push(new element(number(result)));
+					}
+					else
+					{
+						double right_operand_decimal = static_cast<double>(std::get<1>(*right_operand));
+						double left_operand_decimal = std::get<0>(*left_operand).decimal();
+						
+						long long right_operand_integer = *reinterpret_cast<long long*>(&right_operand_decimal);
+						long long left_operand_integer = *reinterpret_cast<long long*>(&left_operand_decimal);
 
-				long long result = left_operand_integer + right_operand_integer;
+						long long result = left_operand_integer + right_operand_integer;
 
-				storage_()->push(new element(number(result)));
+						storage_()->push(new element(number(result)));
+					}
+
+					break;
+				}
+
+				case 1: // 문자 | 숫자 = 숫자
+				{
+					if (is_integer_mode_)
+					{
+						long long right_operand_integer = std::get<0>(*right_operand).integer();
+						long long left_operand_integer = static_cast<long long>(std::get<1>(*left_operand));
+
+						long long result = left_operand_integer | right_operand_integer;
+
+						storage_()->push(new element(number(result)));
+					}
+					else
+					{
+						double right_operand_decimal = std::get<0>(*right_operand).decimal();
+						double left_operand_decimal = static_cast<double>(std::get<1>(*left_operand));
+
+						long long right_operand_integer = *reinterpret_cast<long long*>(&right_operand_decimal);
+						long long left_operand_integer = *reinterpret_cast<long long*>(&left_operand_decimal);
+
+						long long result = left_operand_integer + right_operand_integer;
+
+						storage_()->push(new element(number(result)));
+					}
+
+					break;
+				}
+				}
 			}
 
 			delete right_operand;
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,13 @@ static_assert((sizeof(wchar_t) != sizeof(char32_t) && AHEUIPLUSPLUS_MACRO_IS_WIN
 	(sizeof(wchar_t) == sizeof(char32_t)),
 	"The size of wchar_t and char32_t can be different from each other on Windows.");
 
+#include <Aheuiplusplus/interpreter.hpp>
+
 int main(int argc, char** argv)
 {
+	app::interpreter i(stdin, stdout);
+
+	i.run(U"뱋밯다맿히");
+
 	return 0;
 }
