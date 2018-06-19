2a844682c8b0c5f2566e2f8df7ec777861d53215
static <kmc7468@naver.com>
2018-06-19 18:46:10 +0900

    include/Aheuiplusplus/Aheuiplusplus.hpp 파일 추가
    이 인터프리터를 구성하는 모든 헤더 파일을 include하는 헤더 파일입니다. 미리 컴파일 된 헤더 파일은 아닙니다.

---
+++ include/Aheuiplusplus/Aheuiplusplus.hpp
@@ -0,0 +1,7 @@
+#include <Aheuiplusplus/code.hpp>
+#include <Aheuiplusplus/command_line.hpp>
+#include <Aheuiplusplus/debugger.hpp>
+#include <Aheuiplusplus/function.hpp>
+#include <Aheuiplusplus/interpreter.hpp>
+#include <Aheuiplusplus/storage.hpp>
+#include <Aheuiplusplus/version.hpp>
