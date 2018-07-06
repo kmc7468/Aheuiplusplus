c0daad2dab6e843946d662a9df5a49d3d899acce
static <kmc7468@naver.com>
2018-07-06 23:29:25 +0900

    debugger::dump 메서드의 버그 수정
    저장공간의 모든 요소가 제대로 덤프되지 않던 버그를 수정하였습니다.

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -162,7 +162,9 @@ namespace app
 				std::fprintf(output_stream_, "%s(비어 있음)\n", tab.c_str());
 			}
 
-			for (std::size_t k = 0; k < storage_backup->length(); ++k)
+			std::size_t count = storage_backup->length();
+
+			for (std::size_t k = 0; k < count; ++k)
 			{
 				tab = std::string(depth * 4, ' ');
 
