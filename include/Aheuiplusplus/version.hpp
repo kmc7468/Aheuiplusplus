e9e7df33f384ef53b9347f54c2e828ef1218c6d1
static <kmc7468@naver.com>
2018-08-06 12:52:41 +0900

    version 열거형 추가
    이 열거형은 아희++ 표준안 버전을 나타냅니다.

---
+++ include/Aheuiplusplus/version.hpp
@@ -0,0 +1,26 @@
+#ifndef AHEUIPLUSPLUS_HEADER_VERSION_HPP
+#define AHEUIPLUSPLUS_HEADER_VERSION_HPP
+
+namespace app
+{
+	enum class version
+	{
+		none,
+
+		v1_0,
+		v1_1,
+
+		v2_0,
+
+		latest_v1 = v1_1,
+		latest_v2 = v2_0,
+		latest = latest_v2,
+	};
+
+	int get_major(app::version version) noexcept;
+	int get_minor(app::version version) noexcept;
+	version get_version(int major) noexcept;
+	version get_version(int major, int minor) noexcept;
+}
+
+#endif
---
+++ src/version.cpp
@@ -0,0 +1,80 @@
+#include <Aheuiplusplus/version.hpp>
+
+namespace app
+{
+	int get_major(app::version version) noexcept
+	{
+		switch (version)
+		{
+		case app::version::v1_0:
+		case app::version::v1_1:
+			return 1;
+
+		case app::version::v2_0:
+			return 2;
+
+		default:
+			return -1;
+		}
+	}
+	int get_minor(app::version version) noexcept
+	{
+		switch (version)
+		{
+		case app::version::v1_0:
+		case app::version::v2_0:
+			return 0;
+
+		case app::version::v1_1:
+			return 1;
+
+		default:
+			return -1;
+		}
+	}
+	version get_version(int major) noexcept
+	{
+		switch (major)
+		{
+		case 1:
+			return version::latest_v1;
+
+		case 2:
+			return version::latest_v2;
+
+		default:
+			return version::none;
+		}
+	}
+	version get_version(int major, int minor) noexcept
+	{
+		switch (major)
+		{
+		case 1:
+			switch (minor)
+			{
+			case 0:
+				return version::v1_0;
+
+			case 1:
+				return version::v1_1;
+
+			default:
+				return version::none;
+			}
+
+		case 2:
+			switch (minor)
+			{
+			case 0:
+				return version::v2_0;
+
+			default:
+				return version::none;
+			}
+
+		default:
+			return version::none;
+		}
+	}
+}
