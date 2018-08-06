cbb6a9e2a3a749ea03efcbbda8e1ffc6b7f01113
static <kmc7468@naver.com>
2018-08-06 13:48:17 +0900

    point 클래스 추가
    이 클래스는 아희++ 코드의 한 위치를 저장합니다.

--- include/Aheuiplusplus/cursor.hpp
+++ include/Aheuiplusplus/cursor.hpp
@@ -13,6 +13,30 @@ namespace app
 		down,
 	};
 
+	class point final
+	{
+	public:
+		point() noexcept = default;
+		point(std::size_t x, std::size_t y) noexcept;
+		point(const app::point& point) noexcept;
+		~point() = default;
+
+	public:
+		point& operator=(const app::point& point) noexcept;
+		bool operator==(const app::point& point) const noexcept;
+		bool operator!=(const app::point& point) const noexcept;
+
+	public:
+		std::size_t x() const noexcept;
+		void x(std::size_t new_x) noexcept;
+		std::size_t y() const noexcept;
+		void y(std::size_t new_y) noexcept;
+
+	private:
+		std::size_t x_ = 0;
+		std::size_t y_ = 0;
+	};
+
 	class cursor final
 	{
 	public:
@@ -36,8 +60,7 @@ namespace app
 		void speed(std::size_t new_speed) noexcept;
 
 	private:
-		std::size_t x_ = 0;
-		std::size_t y_ = 0;
+		app::point point_;
 		app::direction direction_ = app::direction::down;
 		std::size_t speed_ = 1;
 	};
--- src/cursor.cpp
+++ src/cursor.cpp
@@ -2,14 +2,56 @@
 
 namespace app
 {
+	point::point(std::size_t x, std::size_t y) noexcept
+		: x_(x), y_(y)
+	{}
+	point::point(const app::point& point) noexcept
+		: x_(point.x_), y_(point.y_)
+	{}
+
+	point& point::operator=(const app::point& point) noexcept
+	{
+		x_ = point.x_;
+		y_ = point.y_;
+		
+		return *this;
+	}
+	bool point::operator==(const app::point& point) const noexcept
+	{
+		return x_ == point.x_ && y_ == point.y_;
+	}
+	bool point::operator!=(const app::point& point) const noexcept
+	{
+		return x_ != point.x_ || y_ != point.y_;
+	}
+
+	std::size_t point::x() const noexcept
+	{
+		return x_;
+	}
+	void point::x(std::size_t new_x) noexcept
+	{
+		x_ = new_x;
+	}
+	std::size_t point::y() const noexcept
+	{
+		return y_;
+	}
+	void point::y(std::size_t new_y) noexcept
+	{
+		y_ = new_y;
+	}
+}
+
+namespace app
+{
 	cursor::cursor(const app::cursor& cursor) noexcept
-		: x_(cursor.x_), y_(cursor.y_), direction_(cursor.direction_), speed_(cursor.speed_)
+		: point_(cursor.point_), direction_(cursor.direction_), speed_(cursor.speed_)
 	{}
 
 	cursor& cursor::operator=(const app::cursor& cursor) noexcept
 	{
-		x_ = cursor.x_;
-		y_ = cursor.y_;
+		point_ = cursor.point_;
 		direction_ = cursor.direction_;
 		speed_ = cursor.speed_;
 
@@ -17,30 +59,30 @@ namespace app
 	}
 	bool cursor::operator==(const app::cursor& cursor) const noexcept
 	{
-		return x_ == cursor.x_ && y_ == cursor.y_ && direction_ == cursor.direction_ &&
+		return point_ == cursor.point_ && direction_ == cursor.direction_ &&
 			   speed_ == cursor.speed_;
 	}
 	bool cursor::operator!=(const app::cursor& cursor) const noexcept
 	{
-		return x_ != cursor.x_ || y_ != cursor.y_ || direction_ != cursor.direction_ ||
+		return point_ != cursor.point_ || direction_ != cursor.direction_ ||
 			   speed_ != cursor.speed_;
 	}
 
 	std::size_t cursor::x() const noexcept
 	{
-		return x_;
+		return point_.x();
 	}
 	void cursor::x(std::size_t new_x) noexcept
 	{
-		x_ = new_x;
+		point_.x(new_x);
 	}
 	std::size_t cursor::y() const noexcept
 	{
-		return y_;
+		return point_.y();
 	}
 	void cursor::y(std::size_t new_y) noexcept
 	{
-		y_ = new_y;
+		point_.y(new_y);
 	}
 	direction cursor::direction() const noexcept
 	{
