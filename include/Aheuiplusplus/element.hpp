1de172817192347d091a18f4e3253a38590e1bf7
static <kmc7468@naver.com>
2018-09-01 23:40:16 +0900

    element 타입 업데이트
    참조를 추가하였습니다.

--- include/Aheuiplusplus/element.hpp
+++ include/Aheuiplusplus/element.hpp
@@ -13,34 +13,51 @@ namespace app
 	enum class element_type
 	{
 		none,
-		array		= 0b100000,
+		array		= 0b0100000,
+		reference	= 0b1000000,
 
-		number		= 0b000001,
-		pointer		= 0b000010,
-		instance	= 0b000100,
-		function	= 0b001000,
-		type		= 0b010000,
+		number		= 0b0000001,
+		pointer		= 0b0000010,
+		instance	= 0b0000100,
+		function	= 0b0001000,
+		type		= 0b0010000,
 
 		array_of_number = number | array,
 		array_of_pointer = pointer | array,
 		array_of_instance = instance | array,
 		array_of_function = function | array,
 		array_of_type = type | array,
+
+		reference_of_number = number | reference,
+		reference_of_pointer = pointer | reference,
+		reference_of_instance = instance | reference,
+		reference_of_function = function | reference,
+		reference_of_type = type | reference,
+
+		reference_of_array_of_number = array_of_number | reference,
+		reference_of_array_of_pointer = array_of_pointer | reference,
+		reference_of_array_of_instance = array_of_instance | reference,
+		reference_of_array_of_function = array_of_function | reference,
+		reference_of_array_of_type = array_of_type | reference,
 	};
 
-	using element_base =
+	using element_element =
 		std::variant<std::variant<long long, double>,	// number
 					 std::uintptr_t,					// pointer
 					 // TODO: instance
-					 function_ptr						// function
-					 // TODO: type
+					 function_ptr,						// function
+					 element_type						// type
 					>;
-
-	using element = std::variant<element_base, std::vector<element_base>>;
+	using element_base = std::variant<element_element, std::vector<element_element>>;
+	using element = std::variant<element_base, element_base*>;
 	using element_ptr = std::shared_ptr<element>;
 
-	element_type get_element_type(const app::element_base& element) noexcept;
-	element_type get_element_type(const app::element& element) noexcept;
+	element_type get_element_type(const element_element& element) noexcept;
+	element_type get_element_type(const element_base& element) noexcept;
+	element_type get_element_type(const element& element) noexcept;
+
+	const element_base& dereference(const element& element) noexcept;
+	element_base& dereference(element& element) noexcept;
 }
 
 #endif
--- src/element.cpp
+++ src/element.cpp
@@ -2,7 +2,7 @@
 
 namespace app
 {
-	element_type get_element_type(const app::element_base& element) noexcept
+	element_type get_element_type(const element_element & element) noexcept
 	{
 		switch (element.index())
 		{
@@ -25,6 +25,19 @@ namespace app
 			return element_type::none;
 		}
 	}
+	element_type get_element_type(const app::element_base& element) noexcept
+	{
+		if (element.index() == 0)
+		{
+			return get_element_type(std::get<0>(element));
+		}
+
+		return static_cast<element_type>(
+			static_cast<int>(
+				get_element_type(std::get<1>(element)[0])
+				) | static_cast<int>(element_type::array)
+			);
+	}
 	element_type get_element_type(const app::element& element) noexcept
 	{
 		if (element.index() == 0)
@@ -34,8 +47,31 @@ namespace app
 
 		return static_cast<element_type>(
 			static_cast<int>(
-					get_element_type(std::get<1>(element)[0])
-				) | static_cast<int>(element_type::array)
+				get_element_type(*std::get<1>(element))
+				) | static_cast<int>(element_type::reference)
 			);
+	}
+
+	const element_base& dereference(const element& element) noexcept
+	{
+		if (element.index() == 0)
+		{
+			return std::get<0>(element);
+		}
+		else
+		{
+			return *std::get<1>(element);
+		}
+	}
+	element_base& dereference(element& element) noexcept
+	{
+		if (element.index() == 0)
+		{
+			return std::get<0>(element);
+		}
+		else
+		{
+			return *std::get<1>(element);
+		}
 	}
 }
