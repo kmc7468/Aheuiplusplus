5540f615e8ef2b62b9520b25d811474fa2a26e55
static <kmc7468@naver.com>
2018-09-01 20:27:05 +0900

    namespace_info 클래스 추가
    이 클래스는 아희++ 코드 내에서 정의된 네임스페이스에 대한 정보를 저장합니다.

---
+++ include/Aheuiplusplus/namespace.hpp
@@ -0,0 +1,45 @@
+﻿#ifndef AHEUIPLUSPLUS_HEADER_NAMESPACE_HPP
+#define AHEUIPLUSPLUS_HEADER_NAMESPACE_HPP
+
+#include <Aheuiplusplus/code.hpp>
+#include <Aheuiplusplus/function.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace app
+{
+	class namespace_info final
+	{
+	public:
+		namespace_info(const code_view& name);
+		namespace_info(code&& name);
+		namespace_info(const namespace_info& namespace_) = delete;
+		namespace_info(namespace_info&& namespace_) noexcept = delete;
+		~namespace_info() = default;
+
+	public:
+		namespace_info& operator=(const namespace_info& namespace_) = delete;
+		namespace_info& operator=(namespace_info&& namespace_) noexcept = delete;
+		bool operator==(const namespace_info& namespace_) const = delete;
+		bool operator!=(const namespace_info& namespace_) const = delete;
+
+	public:
+		code_view name() const;
+
+		const std::vector<std::shared_ptr<namespace_info>>& namespaces() const noexcept;
+		std::vector<std::shared_ptr<namespace_info>>& namespaces() noexcept;
+		const std::vector<function_ptr>& functions() const noexcept;
+		std::vector<function_ptr>& functions() noexcept;
+
+	private:
+		code name_;
+
+		std::vector<std::shared_ptr<namespace_info>> namespaces_;
+		std::vector<function_ptr> functions_;
+	};
+
+	using namespace_ptr = std::shared_ptr<namespace_info>;
+}
+
+#endif
---
+++ src/namespace.cpp
@@ -0,0 +1,35 @@
+﻿#include <Aheuiplusplus/namespace.hpp>
+
+#include <utility>
+
+namespace app
+{
+	namespace_info::namespace_info(const code_view& name)
+		: name_(name)
+	{}
+	namespace_info::namespace_info(code&& name)
+		: name_(std::move(name))
+	{}
+
+	code_view namespace_info::name() const
+	{
+		return name_;
+	}
+
+	const std::vector<std::shared_ptr<namespace_info>>& namespace_info::namespaces() const noexcept
+	{
+		return namespaces_;
+	}
+	std::vector<std::shared_ptr<namespace_info>>& namespace_info::namespaces() noexcept
+	{
+		return namespaces_;
+	}
+	const std::vector<function_ptr>& namespace_info::functions() const noexcept
+	{
+		return functions_;
+	}
+	std::vector<function_ptr>& namespace_info::functions() noexcept
+	{
+		return functions_;
+	}
+}
