33a20ab652b9248d3be1db7ad1d321c911d2eac0
static <kmc7468@naver.com>
2018-09-02 18:54:27 +0900

    벤치마킹 코드 개선
    정밀도를 더 높였습니다.

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 
 #include <chrono>
 #include <cstdlib>
+#include <iomanip>
 #include <ios>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
 #ifdef AHEUIPLUSPLUS_PRINT_BENCHMARK
-	std::clog << std::fixed;
+	std::clog << std::fixed << std::setprecision(10);
 
-	const auto benchmark_timepoint_before_parsing_command_line = std::chrono::system_clock::now();
+	const auto benchmark_timepoint_before_parsing_command_line = std::chrono::high_resolution_clock::now();
 #endif
 
 	app::command_line command_line;
@@ -21,10 +22,12 @@ int main(int argc, char** argv)
 	}
 
 #ifdef AHEUIPLUSPLUS_PRINT_BENCHMARK
-	const auto benchmark_timepoint_after_parsing_command_line = std::chrono::system_clock::now();
-	const std::chrono::duration<double> benchmark_duration_parsing_command_line =
+	const auto benchmark_timepoint_after_parsing_command_line = std::chrono::high_resolution_clock::now();
+	const std::chrono::duration<long double> benchmark_duration_parsing_command_line =
 		benchmark_timepoint_after_parsing_command_line - benchmark_timepoint_before_parsing_command_line;
-	std::clog << "[Benchmark] Parsing command line: " << benchmark_duration_parsing_command_line.count() << "s\n";
+	std::clog << "[Benchmark] Parsing command line: " << benchmark_duration_parsing_command_line.count() << "s(" <<
+		std::chrono::duration_cast<std::chrono::duration<long double, std::milli>>(benchmark_duration_parsing_command_line).count() << "ms, " <<
+		std::chrono::duration_cast<std::chrono::duration<long double, std::micro>>(benchmark_duration_parsing_command_line).count() << "µs)\n";
 #endif
 
 	return EXIT_SUCCESS;
