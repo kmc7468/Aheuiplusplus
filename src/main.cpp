a9f49d73064321cd88bef232a7dc6d5fa5c9e8d2
static <kmc7468@naver.com>
2018-09-02 18:30:42 +0900

    CMakeLists.txt 파일 업데이트
    PRINT_BENCHMARK 캐시를 추가하였습니다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 ﻿#if AHEUIPLUSPLUS_TARGET == 1
 #include <Aheuiplusplus/Aheuiplusplus.hpp>
 
+#include <chrono>
 #include <cstdlib>
+#include <ios>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
+#ifdef AHEUIPLUSPLUS_PRINT_BENCHMARK
+	std::clog << std::fixed;
+
+	const auto benchmark_timepoint_before_parsing_command_line = std::chrono::system_clock::now();
+#endif
+
 	app::command_line command_line;
 	if (!command_line.parse(argc, argv))
 	{
 		return EXIT_FAILURE;
 	}
+
+#ifdef AHEUIPLUSPLUS_PRINT_BENCHMARK
+	const auto benchmark_timepoint_after_parsing_command_line = std::chrono::system_clock::now();
+	const std::chrono::duration<double> benchmark_duration_parsing_command_line =
+		benchmark_timepoint_after_parsing_command_line - benchmark_timepoint_before_parsing_command_line;
+	std::clog << "[Benchmark] Parsing command line: " << benchmark_duration_parsing_command_line.count() << "s\n";
+#endif
 
 	return EXIT_SUCCESS;
 }
